f8c4125de11a3925bad209eb0c2bfbb5863e0ef3
annesarahsad <anne.sadien@spoonconsulting.com>
2025-12-23 04:30:55 +0400

    first commit

---
+++ intern_game.js
@@ -0,0 +1,23 @@
+/**
+ * Git & GitHub Intern Training Game 🎮
+ *
+ * Rules:
+ * 1. Create your own branch (never main)
+ * 2. Add your player below
+ * 3. Do NOT edit other players
+ * 4. Commit, push, and open a Pull Request
+ */
+
+// Example entry
+const players = [
+  {
+    name: "Demo Intern",
+    favoriteCommand: "git status",
+    funFact: "GitHub is not the same as Git"
+  },
+
+  // 👉 Interns: ADD YOUR ENTRY BELOW THIS LINE
+  // (Multiple interns editing here will cause merge conflicts – this is intentional!)
+];
+
+export default players;
